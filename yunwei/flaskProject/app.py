b65403a6b4df147df4fbb53a94acf74a4b805118
test <1614528231@qq.com>
2024-04-28 11:18:22 +0800

    update nginx

--- yunwei/flaskProject/app.py
+++ yunwei/flaskProject/app.py
@@ -3,7 +3,7 @@ import threading
 import time
 import requests
 
-from flask import Flask, render_template, url_for, session, request
+from flask import Flask, render_template, url_for, session, request, jsonify, make_response
 from flaskrr import Get_Host, Get_Rand_ps,Create_Zabbix_Host_1,Mod_domain
 
 class FlaskApp(Flask):
@@ -15,7 +15,7 @@ class FlaskApp(Flask):
         def run_job():
             while True:
                 print('执行后台任务')
-                time.sleep(5)
+                time.sleep(10)
 
         t1 = threading.Thread(target=run_job)
         t1.start()
@@ -75,6 +75,25 @@ def Create_domaindiy():
        tocol = request.form["tocol"]
        result = Mod_domain.ssh_connect_and_execute_commands(IP,file_name,new_domain,port, tocol)
     return render_template('mod_domain.html',result=result)
+# 实现网页写入域名到服务器中-文本形式
+@app.route('/create/domaindiy/file', methods=["GET", "POST"])
+def Create_domaindiy_text():
+    result = None
+    error_message = None
+
+    if request.method == 'POST':
+        file_name = request.form["filename"]
+        nginx_config = request.form["nginx_config"]
+        tocol = request.form["tocol"]
+        print("nginx_config:", nginx_config)
+        try:
+            response = Mod_domain.ssh_connect_and_execute_commands_text(file_name, nginx_config, tocol)
+            if response: 
+                data = response.get_json()
+                error_message = data.get('error','')
+        except Exception as e:
+            error_message = f"SSH connection or command execution error: {e}"
+    return render_template('mod_domain_text.html', error_message=error_message)
 if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=5000, debug=True)
